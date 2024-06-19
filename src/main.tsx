cba55e500ed8ac5ecb49cb8c7386e74a2ba05455
Usman S <ui.sabuwala@gmail.com>
2024-06-19 20:17:28 +0530

    Add Posthog

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import { PostHogProvider } from "posthog-js/react";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <PostHogProvider
+      apiKey={import.meta.env.VITE_POSTHOG_KEY}
+      options={{
+        api_host: import.meta.env.VITE_POSTHOG_HOST,
+      }}
+    >
+      <App />
+    </PostHogProvider>
   </React.StrictMode>,
 );
